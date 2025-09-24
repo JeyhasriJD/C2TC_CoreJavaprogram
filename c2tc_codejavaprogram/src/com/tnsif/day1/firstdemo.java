12c52693a66fb8d5421e77e2e5effd265e7faab2
Admin <Admin@JD>
2025-09-24 15:00:55 +0530

    this project is my example practice of core java program

---
+++ c2tc_codejavaprogram/src/com/tnsif/day1/firstdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.day1;
+
+public class firstdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fullstack java developer course");
+System.out.println("welcome to the tnsif capegemini program by jd");
+System.out.print("ifet college of eng");
+	}
+
+}
---
+++ c2tc_codejavaprogram/src/com/tnsif/day1/identifersdemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.day1;
+
+public class identifersdemo {
+
+	public static void main(String[] args) {
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="anya"; 
+		System.out.println("value is : "+ studentName);
+	}
+
+}
